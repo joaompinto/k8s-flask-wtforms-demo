77d5bc6219d72ab801e482bf233c0934f5cc0965
Joao Pinto <joao.pinto@vshn.net>
2019-11-06 14:26:29 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,52 @@
+#
+# Inspired by:  https://pythonspot.com/flask-web-forms/
+#
+
+
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, validators
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config["SECRET_KEY"] = "afafafafafafafafaf"
+
+
+class ReusableForm(Form):
+    name = TextField("Name:", validators=[validators.InputRequired()])
+    email = TextField(
+        "Email:",
+        validators=[validators.Email(), validators.Length(min=6, max=35)],
+    )
+    password = TextField(
+        "Password:",
+        validators=[validators.InputRequired(), validators.Length(min=3, max=35)],
+    )
+
+    @app.route("/", methods=["GET", "POST"])
+    def hello():
+        form = ReusableForm(request.form)
+
+        print(form.errors)
+        if request.method == "POST":
+            print("ON POST")
+            name = request.form["name"]
+            password = request.form["password"]
+            email = request.form["email"]
+            print(name, " ", email, " ", password)
+
+            if form.validate():
+                # Save the comment here.
+                flash("Thanks for registration " + name)
+            else:
+                for error_field, error_reason in form.errors.items():
+                    print(f"Error: {error_field} {error_reason}")
+                    flash(f"Error: {error_field} {error_reason}")
+
+
+        return render_template("app.html", form=form)
+
+
+if __name__ == "__main__":
+    app.run()
